42700f951c0fc5380b26491c1008f6d7888a3614
durosly <slyboydon1@gmail.com>
2023-08-05 11:54:18 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,jsx}",
+		"./components/**/*.{js,jsx}",
+		"./app/**/*.{js,jsx}",
+		"./src/**/*.{js,jsx}",
+	],
+	plugins: [require("daisyui"), require("tailwindcss-animate")],
+	daisyui: {
+		themes: [
+			{
+				emerald: {
+					...require("daisyui/src/theming/themes")[
+						"[data-theme=emerald]"
+					],
+					primary: "#00eaaf",
+				},
+			},
+		],
+	},
+};
