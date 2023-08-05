a7d3f75efd145872f0404e5beaacec881cf62a21
durosly <slyboydon1@gmail.com>
2023-08-05 11:54:46 +0100

    Create header.jsx

---
+++ app/(public)/components/header.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Nav from "./nav";
+import logo from "@/images/logo.png";
+
+function Header() {
+	return (
+		<div className="w-full navbar border-b">
+			<div className="flex-none lg:hidden">
+				<label
+					htmlFor="my-drawer-3"
+					className="btn btn-square btn-ghost"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						className="inline-block w-6 h-6 stroke-current"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						></path>
+					</svg>
+				</label>
+			</div>
+			<div className="flex-1 flex px-2 mx-2">
+				<div className="w-5 h-5 relative">
+					<Image
+						src={logo}
+						alt=""
+						placeholder="blur"
+						fill
+					/>
+				</div>
+				<div>Clatras</div>
+			</div>
+			<div className="flex-none hidden lg:block">
+				<ul className="menu menu-horizontal">
+					{/* Navbar menu content here */}
+					<Nav />
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
