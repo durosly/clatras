02a6700feb1dbd49a3706ff17f3d964a0307f0df
durosly <slyboydon1@gmail.com>
2023-08-05 11:54:59 +0100

    Create wrapper.jsx

---
+++ app/(public)/components/wrapper.jsx
@@ -0,0 +1,50 @@
+import logo from "@/images/logo.png";
+import Image from "next/image";
+import Footer from "./footer";
+import Header from "./header";
+import Nav from "./nav";
+
+function Wrapper({ children }) {
+	return (
+		<div className="drawer">
+			<input
+				id="my-drawer-3"
+				type="checkbox"
+				className="drawer-toggle"
+			/>
+			<div className="drawer-content flex flex-col">
+				{/* Navbar */}
+				<Header />
+				{/* Page content here */}
+				{children}
+
+				{/* footer */}
+				<Footer />
+			</div>
+			<div className="drawer-side">
+				<label
+					htmlFor="my-drawer-3"
+					className="drawer-overlay"
+				></label>
+
+				<ul className="menu p-4 w-80 h-full bg-base-200">
+					{/* Sidebar content here */}
+					<li className="menu-title flex flex-row items-center gap-2">
+						<div className="w-8 h-8 relative">
+							<Image
+								src={logo}
+								alt=""
+								placeholder="blur"
+								fill
+							/>
+						</div>
+						<div>Clatras</div>
+					</li>
+					<Nav />
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Wrapper;
