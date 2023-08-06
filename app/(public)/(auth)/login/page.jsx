c22759ab8a9c0677bb8bf45f9346189dc628604e
durosly <slyboydon1@gmail.com>
2023-08-06 05:52:43 +0100

    Update page.jsx

--- app/(public)/(auth)/login/page.jsx
+++ app/(public)/(auth)/login/page.jsx
@@ -1,45 +1,12 @@
 import Link from "next/link";
 import { AiOutlineEye } from "react-icons/ai";
+import LoginForm from "./login-form";
 
 function LoginPage() {
 	return (
 		<div>
 			<h1 className="text-2xl font-bold text-center mb-5">Login page</h1>
-			<form>
-				<div className="form-control ">
-					<label className="label">
-						<span className="label-text">E-mail</span>
-					</label>
-					<input
-						type="email"
-						placeholder="E-mail..."
-						className="input input-bordered w-full"
-					/>
-				</div>
-				<div className="form-control ">
-					<label className="label">
-						<span className="label-text">Password</span>
-					</label>
-					<input
-						type="password"
-						placeholder="Password..."
-						className="input input-bordered w-full"
-					/>
-					<div className="text-right">
-						<button
-							type="button"
-							className="btn btn-xs btn-ghost"
-						>
-							<AiOutlineEye />
-						</button>
-					</div>
-				</div>
-				<div className="mt-5">
-					<button className="btn btn-primary sm:btn-block">
-						Log in
-					</button>
-				</div>
-			</form>
+			<LoginForm />
 			<p className="mt-5">
 				Don&apos;t have an account?{" "}
 				<Link
