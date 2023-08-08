65b9d96e501369824b6af341abd8a6abb557b21c
durosly <slyboydon1@gmail.com>
2023-08-08 03:04:06 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,14 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
+	const token = request.cookies.get(process.env.NEXT_PUBLIC_COOKIE_AUTH_KEY);
+
+	console.log(token);
+	if (!token) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
+
+	// const account = clientServer
 	// if (request.nextUrl.pathname.startsWith('/about')) {
 	//   return NextResponse.rewrite(new URL('/about-2', request.url))
 	// }
