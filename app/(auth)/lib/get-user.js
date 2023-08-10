6404052f07c693944818c672411fdd52cd35d0f7
durosly <slyboydon1@gmail.com>
2023-08-10 17:47:46 +0100

    📌update getActiveUser handler to utilize just user ID

---
+++ app/(auth)/lib/get-user.js
@@ -0,0 +1,19 @@
+import { cookies } from "next/headers";
+
+async function getActiveUser() {
+	try {
+		const cookieStore = cookies();
+		const userId = cookieStore.get(process.env.NEXT_PUBLIC_COOKIE_AUTH_KEY);
+
+		if (userId) {
+			return userId;
+		}
+
+		throw new Error("No session found");
+	} catch (error) {
+		console.log("error", error);
+		return null;
+	}
+}
+
+export default getActiveUser;
