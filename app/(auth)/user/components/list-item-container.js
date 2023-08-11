c67907d02418e0cf1228247ee78529a4d44bd8f9
durosly <slyboydon1@gmail.com>
2023-08-11 16:13:29 +0100

    ✅create transaction history list item container

---
+++ app/(auth)/user/components/list-item-container.js
@@ -0,0 +1,36 @@
+import getActiveUser from "@/app/(auth)/lib/get-user";
+import clientServer from "@/lib/client-server";
+import { Databases, Query } from "node-appwrite";
+import ListItem from "./list-item";
+
+async function ListItemContainer() {
+	clientServer.setKey(process.env.APPWRITE_API_KEY);
+	const databases = new Databases(clientServer);
+	const userId = await getActiveUser();
+
+	const documents = await databases.listDocuments(
+		process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+		process.env.NEXT_PUBLIC_APPRWRITE_TRANSACTION_COLLECTION_ID,
+		[
+			Query.equal("userId", userId.value),
+			Query.limit(5),
+			Query.orderAsc("$createdAt"),
+		]
+	);
+
+	console.log(documents);
+
+	return (
+		<tbody>
+			{documents.documents.map((d, i) => (
+				<ListItem
+					key={d.$id}
+					doc={d}
+					count={i + 1}
+				/>
+			))}
+		</tbody>
+	);
+}
+
+export default ListItemContainer;
