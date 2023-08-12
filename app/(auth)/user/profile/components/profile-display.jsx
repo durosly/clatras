0fa677e83ca77f26771fd785e75923de67fd4385
durosly <slyboydon1@gmail.com>
2023-08-12 14:40:30 +0100

    Update profile-display.jsx

--- app/(auth)/user/profile/components/profile-display.jsx
+++ app/(auth)/user/profile/components/profile-display.jsx
@@ -1,8 +1,49 @@
 "use client";
-
+import { appwriteClient } from "@/lib/client";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import { FaUser } from "react-icons/fa";
+import ProfileImgDisplay from "./profile-img-display";
 
 function ProfileDisplay() {
+	const [user, setUser] = useState(null);
+	const [isSending, setIsSending] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		async function loadProfile() {
+			try {
+				const account = await appwriteClient.getCurrentUser();
+				if (account) {
+					setUser(account);
+				}
+				// console.log(account);
+			} catch (error) {
+				toast.error(error.message);
+				console.log(error);
+			} finally {
+				setIsLoading(false);
+			}
+		}
+
+		loadProfile();
+	}, []);
+
+	async function sendEmail() {
+		try {
+			if (isSending) return;
+
+			setIsSending(true);
+
+			const token = await appwriteClient.sendVerificationEmail();
+
+			toast.success("email sent");
+		} catch (error) {
+			toast.error(error.message);
+		} finally {
+			setIsSending(false);
+		}
+	}
+
 	return (
 		<div className="border p-5 rounded-md">
 			<div className="flex gap-2 items-center">
@@ -11,21 +52,55 @@ function ProfileDisplay() {
 				</span>
 				<h2 className="font-bold">Profile</h2>
 			</div>
-			<div className="mt-4">
-				<div className="flex justify-between flex-wrap gap-2">
-					<div>
-						<h2>Albert Duro</h2>
-						<p>Joined: Aug 9, 2023, 23:47</p>
+			{isLoading ? (
+				<span className="loading loading-ball"></span>
+			) : (
+				user && (
+					<div className="mt-4">
+						<div className="flex justify-between flex-wrap gap-2">
+							<div className="flex gap-2 items-center">
+								<div className=" avatar">
+									<ProfileImgDisplay />
+								</div>
+								<h2 className="text-xl">{user.name}</h2>
+							</div>
+							<div className="space-y-2">
+								<p>
+									{user.email}, {user.phone}
+								</p>
+								<span className="badge badge-neutral">
+									{user.emailVerification
+										? "Verified"
+										: "Unverified"}
+								</span>
+								<p className="text-xs">
+									Joined:{" "}
+									{new Intl.DateTimeFormat("en-GB", {
+										dateStyle: "full",
+										timeStyle: "long",
+										hour12: true,
+									}).format(new Date(user.registration))}
+								</p>
+							</div>
+						</div>
+						{!user.emailVerification && (
+							<div className="mt-2">
+								<button
+									disabled={isSending}
+									className="btn btn-neutral"
+									onClick={sendEmail}
+								>
+									{isSending ? (
+										<span className="loading loading-dots"></span>
+									) : (
+										"Verify Account"
+									)}
+								</button>
+							</div>
+						)}
 					</div>
-					<div>
-						<p>slyboydon1@gmail.com, 23463069903</p>
-						<span className="badge badge-neutral">Unverified</span>
-					</div>
-				</div>
-				<div className="mt-2">
-					<button className="btn btn-neutral">Verify Account</button>
-				</div>
-			</div>
+				)
+			)}
 		</div>
 	);
 }
