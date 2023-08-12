d10f6fca7196fd356d635e8059b7267c6aa8440f
durosly <slyboydon1@gmail.com>
2023-08-12 05:04:48 +0100

    Update logout-btn.jsx

--- app/(auth)/components/logout-btn.jsx
+++ app/(auth)/components/logout-btn.jsx
@@ -1,20 +1,12 @@
 "use client";
 import { deleteCookie } from "cookies-next";
+import { appwriteClient } from "@/lib/client";
 import { useRouter } from "next/navigation";
-// import { Client, Account } from "appwrite";
 
 function LogoutButton() {
-	// const client = new Client();
-
-	// client
-	// 	.setEndpoint(process.env.NEXT_PUBLIC_APPRWRITE_ENDPOINT)
-	// 	.setProject(process.env.NEXT_PUBLIC_APPRWRITE_PROJECT_ID);
-
-	// const account = new Account(client);
-
 	const router = useRouter();
 	async function handleLogout() {
-		// await account.deleteSessions("current");
+		await appwriteClient.logoutCurrent();
 		deleteCookie(process.env.NEXT_PUBLIC_COOKIE_AUTH_KEY, {
 			path: "/user",
 			domain: "localhost",
