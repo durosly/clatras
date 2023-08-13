339b7209e38ba471fd3b3bf6f45ee1d245fe9277
durosly <slyboydon1@gmail.com>
2023-08-13 15:40:39 +0100

    🗑️remove directing

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,13 +1,5 @@
-import { redirect } from "next/navigation";
 import Wrapper from "./components/wrapper";
-import getActiveUser from "./lib/get-user";
 
 export default async function AuthLayout({ children }) {
-	let user = await getActiveUser();
-
-	if (!user) {
-		redirect("/login");
-	}
-
 	return <Wrapper>{children}</Wrapper>;
 }
