827a910b39fac44ad4f9ad95b7e698fc360f9148
durosly <slyboydon1@gmail.com>
2023-08-13 13:42:07 +0100

    📌add bank details display

--- app/(auth)/user/profile/page.jsx
+++ app/(auth)/user/profile/page.jsx
@@ -1,11 +1,12 @@
-import { FaUser } from "react-icons/fa";
 import ProfileDisplay from "./components/profile-display";
 import PasswordUpdate from "./components/password-update";
+import BankDetails from "./components/bank-details";
 
 function UserProfilePage() {
 	return (
-		<div className="mx-5 sm:mx-10 py-5 space-y-4">
+		<div className="mx-5 sm:mx-10 py-5 space-y-10">
 			<ProfileDisplay />
+			<BankDetails />
 			<PasswordUpdate />
 		</div>
 	);
