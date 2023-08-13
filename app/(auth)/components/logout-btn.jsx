fc5c143410b462e05ddea1a48b19e7fb7b531cdf
durosly <slyboydon1@gmail.com>
2023-08-14 00:21:25 +0100

    📌use next-auth signout on logout button

--- app/(auth)/components/logout-btn.jsx
+++ app/(auth)/components/logout-btn.jsx
@@ -1,17 +1,14 @@
 "use client";
-import { deleteCookie } from "cookies-next";
 import { appwriteClient } from "@/lib/client";
 import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
 
 function LogoutButton() {
 	const router = useRouter();
 	async function handleLogout() {
 		await appwriteClient.logoutCurrent();
-		deleteCookie(process.env.NEXT_PUBLIC_COOKIE_AUTH_KEY, {
-			path: "/user",
-			domain: "localhost",
-		});
-		router.push("/login");
+		const data = await signOut({ redirect: false, callbackUrl: "/login" });
+		router.push(data.url);
 	}
 
 	return <a onClick={handleLogout}>Logout</a>;
