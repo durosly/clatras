1a6d108738d5e0a9222f199c36d0e1e1db1de58d
durosly <slyboydon1@gmail.com>
2023-08-14 01:50:02 +0100

    📌update next-auth options
    - isAdmin: account.prefs?.isAdmin === "true" ? true : false,
    + isAdmin: account.prefs?.isAdmin === true ? true : false,

--- app/api/auth/options.js
+++ app/api/auth/options.js
@@ -28,7 +28,7 @@ export const authOptions = {
 				return {
 					userId: account.$id,
 					name: account.name,
-					isAdmin: account.prefs?.isAdmin === "true" ? true : false,
+					isAdmin: account.prefs?.isAdmin === true ? true : false,
 				};
 			},
 		}),
