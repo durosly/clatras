b551bede0be8016fbc62148c72718cbadb3480cf
durosly <slyboydon1@gmail.com>
2023-08-14 02:36:53 +0100

    Update list-item-container.js

--- app/(auth)/user/components/list-item-container.js
+++ app/(auth)/user/components/list-item-container.js
@@ -2,17 +2,21 @@ import getActiveUser from "@/app/(auth)/lib/get-user";
 import clientServer from "@/lib/client-server";
 import { Databases, Query } from "node-appwrite";
 import ListItem from "./list-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth/options";
 
 async function ListItemContainer() {
+	const session = await getServerSession(authOptions);
+
 	clientServer.setKey(process.env.APPWRITE_API_KEY);
 	const databases = new Databases(clientServer);
-	const userId = await getActiveUser();
+	const userId = session.user.userId;
 
 	const documents = await databases.listDocuments(
 		process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
 		process.env.NEXT_PUBLIC_APPRWRITE_TRANSACTION_COLLECTION_ID,
 		[
-			Query.equal("userId", userId.value),
+			Query.equal("userId", userId),
 			Query.limit(5),
 			Query.orderDesc("$createdAt"),
 		]
