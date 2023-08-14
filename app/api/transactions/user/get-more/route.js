568cd5cb3d7497055a784de9e084efe5ac98d45f
durosly <slyboydon1@gmail.com>
2023-08-14 04:37:46 +0100

    📌change from cookie to next-auth

--- app/api/transactions/user/get-more/route.js
+++ app/api/transactions/user/get-more/route.js
@@ -1,30 +1,33 @@
-import clientServer from "@/lib/client-server";
-import { cookies } from "next/headers";
+import { AppwriteServerClient } from "@/lib/client-server";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { Databases, Query } from "node-appwrite";
+import { authOptions } from "@/auth/options";
 
 async function getNextTransactions(request) {
 	try {
+		const session = await getServerSession(authOptions);
+		const userId = session.user.userId;
+
 		const { searchParams } = new URL(request.url);
 		const lastId = searchParams.get("lastId");
-		const cookieStore = cookies();
-		const cookie = cookieStore.get(process.env.NEXT_PUBLIC_COOKIE_AUTH_KEY);
-		if (!cookie?.value) {
-			throw new Error("Invalid access");
-		}
+		const token = searchParams.get("token");
 
 		if (!lastId) throw new Error("No ID specified");
+		if (!token) throw new Error("Token error");
 
-		clientServer.setKey(process.env.APPWRITE_API_KEY);
+		const app = new AppwriteServerClient();
+		app.setToken(token);
+		const server = app.getServer();
 
-		const databases = new Databases(clientServer);
+		const databases = new Databases(server);
 
 		let databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
 		let collectionId =
 			process.env.NEXT_PUBLIC_APPRWRITE_TRANSACTION_COLLECTION_ID;
 
 		const doc = await databases.listDocuments(databaseId, collectionId, [
-			Query.equal("userId", cookie.value),
+			Query.equal("userId", userId),
 			Query.limit(5),
 			Query.orderDesc("$createdAt"),
 			Query.cursorAfter(lastId),
