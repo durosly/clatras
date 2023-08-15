31098a3ac068cbd2cf118d3f50f448c65319dc63
durosly <slyboydon1@gmail.com>
2023-08-16 00:35:33 +0100

    🗑️remove border bottom from header

--- app/(public)/components/header.jsx
+++ app/(public)/components/header.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 function Header() {
 	return (
-		<div className="w-full navbar border-b">
+		<div className="w-full navbar ">
 			<div className="flex-none lg:hidden">
 				<label
 					htmlFor="my-drawer-3"
