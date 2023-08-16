f745b9d19883ef8b8292fa4f0ecacc4edeebc995
durosly <slyboydon1@gmail.com>
2023-08-16 01:29:57 +0100

    Create loading.jsx

---
+++ app/(auth)/user/exchange/[type]/loading.jsx
@@ -0,0 +1,14 @@
+function Loading() {
+	return (
+		<div className="max-w-[400px] mx-auto mt-10 border px-8 rounded-md py-10 space-y-10 mb-10">
+			<div className="text-center">
+				<h2 className="uppercase font-bold bg-slate-400 animate-pulse">
+					&nbsp;
+				</h2>
+			</div>
+			<div className="h-10 bg-slate-400 animate-pulse"></div>
+		</div>
+	);
+}
+
+export default Loading;
