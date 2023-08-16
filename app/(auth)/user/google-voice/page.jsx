00efe13d98303ab9dab520701e6d241e19f17ff6
durosly <slyboydon1@gmail.com>
2023-08-16 01:29:28 +0100

    ✅add email verification constraint

--- app/(auth)/user/google-voice/page.jsx
+++ app/(auth)/user/google-voice/page.jsx
@@ -1,8 +1,13 @@
-import { Databases } from "node-appwrite";
+import { Databases, Users } from "node-appwrite";
 import PurchaseDisplay from "./components/purchase-display";
 import clientServer from "@/lib/client-server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/options";
+import Link from "next/link";
 
 async function GoogleVoicPurchasePage() {
+	const session = await getServerSession(authOptions);
+	const userId = session.user.userId;
 	clientServer.setKey(process.env.APPWRITE_API_KEY);
 	const database = new Databases(clientServer);
 	let database_id = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
@@ -14,12 +19,28 @@ async function GoogleVoicPurchasePage() {
 		"64d6828391e8d5b522e8"
 	);
 
+	const users = new Users(clientServer);
+
+	const user = await users.get(userId);
+
 	return (
-		<div className="max-w-[400px] mx-auto mt-10 border px-10 rounded-md py-10 space-y-10 mb-10">
+		<div className="max-w-[400px] mx-auto mt-10 border px-8 rounded-md py-10 space-y-10 mb-10">
 			<div className="text-center">
 				<h2 className="uppercase font-bold">Google Voice</h2>
 			</div>
-			<PurchaseDisplay doc={doc} />
+			{user.emailVerification ? (
+				<PurchaseDisplay doc={doc} />
+			) : (
+				<div className="text-center space-y-4">
+					<p>Please, verify your email address to continue</p>
+					<Link
+						className="btn btn-primary"
+						href="/user/profile"
+					>
+						Profile
+					</Link>
+				</div>
+			)}
 		</div>
 	);
 }
