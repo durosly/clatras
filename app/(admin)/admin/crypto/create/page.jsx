cbcfaa09ef941e8f81cf33b71083036c166d4c64
durosly <slyboydon1@gmail.com>
2023-09-01 17:32:21 +0100

    📌opt out of caching

--- app/(admin)/admin/crypto/create/page.jsx
+++ app/(admin)/admin/crypto/create/page.jsx
@@ -1,6 +1,9 @@
 import CreateCryptoForm from "./create-crypto-form";
 import CoinMarketCap from "coinmarketcap-api";
 
+// Opt out of caching for all data requests in the route segment
+export const dynamic = "force-dynamic";
+
 async function AdminCreateCryptoPage() {
 	const client = new CoinMarketCap(process.env.COINMARKETCAP_API);
 	const mapData = await client.getIdMap({ sort: "cmc_rank", limit: 100 });
--- app/api/admin/crypto/route.js
+++ app/api/admin/crypto/route.js
@@ -2,4 +2,6 @@ import createCrypto from "./createCrypto";
 import updateCrypto from "./update-crypto";
 import deleteCrypto from "./deleteCrypto";
 
+export const revalidate = 0;
+
 export { createCrypto as POST, updateCrypto as PUT, deleteCrypto as DELETE };
