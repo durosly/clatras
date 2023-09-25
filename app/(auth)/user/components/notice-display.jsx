aaec44026b87f4964bc2e45033937d3e863f186c
durosly <slyboydon1@gmail.com>
2023-09-25 11:47:34 +0100

    📌sepearate notice from welcome message

--- app/(auth)/user/components/notice-display.jsx
+++ app/(auth)/user/components/notice-display.jsx
@@ -25,39 +25,38 @@ function NoticeDisplay() {
 	};
 
 	return (
-		<div className="space-y-2">
-			{error || isLoading || !data ? (
-				<div className="bg-primary/10 p-5 rounded-2xl">
-					<h2 className="text-xl font-bold">Welcome to Clatras</h2>
-					<p className="text-sm">
-						Experience fast and easy transactions
-					</p>
-				</div>
-			) : (
-				data.notice.documents.map((n) => (
-					<div
-						key={n.$id}
-						className={`alert ${bg[n.type]} bg-opacity-60 ${
-							alertType[n.type]
-						}`}
-					>
-						{n.type === "success" ? (
-							<BsPatchCheck className="w-6 h-6" />
-						) : n.type === "warning" ? (
-							<VscWarning className="w-6 h-6" />
-						) : n.type === "error" ? (
-							<MdWarningAmber className="w-6 h-6" />
-						) : (
-							<TbSpeakerphone className="w-6 h-6" />
-						)}
-						<div>
-							<h3 className="font-bold">{n.title}</h3>
-							<div className="text-xs">{n.message}</div>
-						</div>
-					</div>
-				))
-			)}
-		</div>
+		<>
+			<div className="bg-primary/10 p-5 rounded-2xl">
+				<h2 className="text-xl font-bold">Welcome to Clatras</h2>
+				<p className="text-sm">Experience fast and easy transactions</p>
+			</div>
+			<div className="space-y-2">
+				{error || isLoading || !data || data?.notice?.total === 0
+					? null
+					: data.notice.documents.map((n) => (
+							<div
+								key={n.$id}
+								className={`alert ${bg[n.type]} bg-opacity-60 ${
+									alertType[n.type]
+								}`}
+							>
+								{n.type === "success" ? (
+									<BsPatchCheck className="w-6 h-6" />
+								) : n.type === "warning" ? (
+									<VscWarning className="w-6 h-6" />
+								) : n.type === "error" ? (
+									<MdWarningAmber className="w-6 h-6" />
+								) : (
+									<TbSpeakerphone className="w-6 h-6" />
+								)}
+								<div>
+									<h3 className="font-bold">{n.title}</h3>
+									<div className="text-xs">{n.message}</div>
+								</div>
+							</div>
+					  ))}
+			</div>
+		</>
 	);
 }
 
